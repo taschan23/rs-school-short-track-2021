53f728b351b8a53a23887c3f1b0e9441f4b0e3bd
evgeny <bakalov.es@ya.ru>
2021-04-12 21:09:06 +0300

    feat: finish task 13

--- src/13-stack.js
+++ src/13-stack.js
@@ -11,16 +11,20 @@
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new Error('Not implemented');
+  constructor() {
+    this.stackArr = [];
+  }
+
+  push(element) {
+    this.stackArr.push(element);
   }
 
   pop() {
-    throw new Error('Not implemented');
+    return this.stackArr.pop();
   }
 
   peek() {
-    throw new Error('Not implemented');
+    return this.stackArr[this.stackArr.length - 1];
   }
 }
 
