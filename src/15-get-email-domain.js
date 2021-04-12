b97a701bc552d53667b48d6289202050c8943281
evgeny <bakalov.es@ya.ru>
2021-04-12 21:14:19 +0300

    feat: finish task 15

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,9 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  return email.split('@')
+    .pop();
 }
 
 module.exports = getEmailDomain;
