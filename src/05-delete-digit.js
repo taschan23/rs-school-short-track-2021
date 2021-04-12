91f3f7434632d39bbc5e9683c98229c55fea7537
evgeny <bakalov.es@ya.ru>
2021-04-12 18:00:42 +0300

    feat: finish task 05

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,25 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  let arr = n.toString()
+    .split('');
+  let res = [];
+  let min = Math.min.apply(null, arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    let current = parseInt(arr[i]);
+    if (current === min) {
+      if (current === parseInt(arr[i - 1])) {
+        res.push(current);
+      }
+    } else {
+      res.push(current);
+    }
+  }
+
+  return Number(res.join(''));
+
 }
 
 module.exports = deleteDigit;
